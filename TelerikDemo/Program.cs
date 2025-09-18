f1ca22e35ad192115dc9fb2a943b671af21a3ba9
Philip Morley <philip.morley@codurance.com>
2025-09-18 15:23:16 +0100

    Initial commit

---
+++ TelerikDemo/Program.cs
@@ -0,0 +1,37 @@
+using TelerikDemo.Client.Pages;
+using TelerikDemo.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddTelerikBlazor();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(TelerikDemo.Client._Imports).Assembly);
+
+app.Run();
+
+throw new ApplicationException("Test Error");
